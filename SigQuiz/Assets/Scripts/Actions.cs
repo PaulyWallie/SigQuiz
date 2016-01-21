a9a7a682b466f4f5059972cb990d38fae3c11b09
Paul Kantoski <unkleyoyo@yahoo.com>
2016-01-20 20:04:30 -0500

    A very long time consuming process complete
    I took a break from 12/252015 to 1/20/2016 I was sick for a part of that
    time. I made a simple scoring system. Then I made changes to my action
    script, instead of a reward and lose methods for general purpose.  Now
    there is a method for each reward and lose condition. I made it as
    simple as possible!! during the long process I found some errors in two
    of my questions, and made the changes on the question document as well.
    I also Found a copy of an answer for one of my questions fixed that as
    well on the game side. Lastly I connected the question buttons to the
    questions. Phew!! that took hours!!! It was fun!!!

--- SigQuiz/Assets/Scripts/Actions.cs
+++ SigQuiz/Assets/Scripts/Actions.cs
@@ -13,61 +13,530 @@ public class Actions : MonoBehaviour {
 	[SerializeField]
 	Button[] questionButton;
 	
-	
-	public void ShowGenaral100(){
+	public void General100(){
 		questionPanel.SetActive (true);
 		mainCanvas.gameObject.SetActive(false);
 		questions[0].gameObject.SetActive (true);
 		questionButton[0].gameObject.SetActive (false);
 	}
 	
-	public void Reward100(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+	public void General200(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[1].gameObject.SetActive (true);
+		questionButton[1].gameObject.SetActive (false);
 	}
-	
-	public void Reward200(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void General300(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[2].gameObject.SetActive (true);
+		questionButton[2].gameObject.SetActive (false);
 	}
-	
-	public void Reward300(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void General400(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[3].gameObject.SetActive (true);
+		questionButton[3].gameObject.SetActive (false);
 	}
-	
-	public void Reward400(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void General500(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[4].gameObject.SetActive (true);
+		questionButton[4].gameObject.SetActive (false);
 	}
-	
-	public void Reward500(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void Founders100(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[5].gameObject.SetActive (true);
+		questionButton[5].gameObject.SetActive (false);
 	}
-	
-	public void Lose100(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void Founders200(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[6].gameObject.SetActive (true);
+		questionButton[6].gameObject.SetActive (false);
 	}
-	
-	public void Lose200(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void Founders300(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[7].gameObject.SetActive (true);
+		questionButton[7].gameObject.SetActive (false);
 	}
-	
-	public void Lose300(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void Founders400(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[8].gameObject.SetActive (true);
+		questionButton[8].gameObject.SetActive (false);
 	}
-	
-	public void Lose400(){
-		questionPanel.SetActive (false);
-		mainCanvas.gameObject.SetActive(true);
+
+	public void Founders500(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[9].gameObject.SetActive (true);
+		questionButton[9].gameObject.SetActive (false);
 	}
+
+	public void TheJordenStandard100(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[10].gameObject.SetActive (true);
+		questionButton[10].gameObject.SetActive (false);
+	}
+
+	public void TheJordenStandard200(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[11].gameObject.SetActive (true);
+		questionButton[11].gameObject.SetActive (false);
+	}
+
+	public void TheJordenStandard300(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[12].gameObject.SetActive (true);
+		questionButton[12].gameObject.SetActive (false);
+	}
+
+	public void TheJordenStandard400(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[13].gameObject.SetActive (true);
+		questionButton[13].gameObject.SetActive (false);
+	}
+
+	public void TheJordenStandard500(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[14].gameObject.SetActive (true);
+		questionButton[14].gameObject.SetActive (false);
+	}
+
+	public void TheSigmaChiCreed100(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[15].gameObject.SetActive (true);
+		questionButton[15].gameObject.SetActive (false);
+	}
+
+	public void TheSigmaChiCreed200(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[16].gameObject.SetActive (true);
+		questionButton[16].gameObject.SetActive (false);
+	}
+
+	public void TheSigmaChiCreed300(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[17].gameObject.SetActive (true);
+		questionButton[17].gameObject.SetActive (false);
+	}
+
+	public void TheSigmaChiCreed400(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[18].gameObject.SetActive (true);
+		questionButton[18].gameObject.SetActive (false);
+	}
+
+	public void TheSigmaChiCreed500(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[19].gameObject.SetActive (true);
+		questionButton[19].gameObject.SetActive (false);
+	}
+
+	public void TheSpiritOfSigmaChi100(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[20].gameObject.SetActive (true);
+		questionButton[20].gameObject.SetActive (false);
+	}
+
+	public void TheSpiritOfSigmaChi200(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[21].gameObject.SetActive (true);
+		questionButton[21].gameObject.SetActive (false);
+	}
+
+	public void TheSpiritOfSigmaChi300(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[22].gameObject.SetActive (true);
+		questionButton[22].gameObject.SetActive (false);
+	}
+
+	public void TheSpiritOfSigmaChi400(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[23].gameObject.SetActive (true);
+		questionButton[23].gameObject.SetActive (false);
+	}
+
+	public void TheSpiritOfSigmaChi500(){
+		questionPanel.SetActive (true);
+		mainCanvas.gameObject.SetActive(false);
+		questions[24].gameObject.SetActive (true);
+		questionButton[24].gameObject.SetActive (false);
+	}
+
 	
-	public void Lose500(){
+	public void GeneralReward100(){
+		KnowledgePoints.knowledgePoints += 100;
 		questionPanel.SetActive (false);
 		mainCanvas.gameObject.SetActive(true);
+		questions[0].SetActive(false);
+	}
+
+	public void GeneralReward200(){
+		KnowledgePoints.knowledgePoints += 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[1].SetActive(false);
+	}
+
+	public void GeneralReward300(){
+		KnowledgePoints.knowledgePoints += 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[2].SetActive(false);
+	}
+
+	public void GeneralReward400(){
+		KnowledgePoints.knowledgePoints += 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[3].SetActive(false);
+	}
+
+	public void GeneralReward500(){
+		KnowledgePoints.knowledgePoints += 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[4].SetActive(false);
+	}
+
+	public void FoundersReward100(){
+		KnowledgePoints.knowledgePoints += 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[5].SetActive(false);
+	}
+
+	public void FoundersReward200(){
+		KnowledgePoints.knowledgePoints += 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[6].SetActive(false);
+	}
+
+	public void FoundersReward300(){
+		KnowledgePoints.knowledgePoints += 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[7].SetActive(false);
+	}
+
+	public void FoundersReward400(){
+		KnowledgePoints.knowledgePoints += 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[8].SetActive(false);
+	}
+
+	public void FoundersReward500(){
+		KnowledgePoints.knowledgePoints += 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[9].SetActive(false);
+	}
+
+	public void TheJordenStandardReward100(){
+		KnowledgePoints.knowledgePoints += 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[10].SetActive(false);
+	}
+
+	public void TheJordenStandardReward200(){
+		KnowledgePoints.knowledgePoints += 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[11].SetActive(false);
+	}
+
+	public void TheJordenStandardReward300(){
+		KnowledgePoints.knowledgePoints += 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[12].SetActive(false);
+	}
+
+	public void TheJordenStandardReward400(){
+		KnowledgePoints.knowledgePoints += 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[13].SetActive(false);
+	}
+
+	public void TheJordenStandardReward500(){
+		KnowledgePoints.knowledgePoints += 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[14].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedReward100(){
+		KnowledgePoints.knowledgePoints += 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[15].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedReward200(){
+		KnowledgePoints.knowledgePoints += 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[16].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedReward300(){
+		KnowledgePoints.knowledgePoints += 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[17].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedReward400(){
+		KnowledgePoints.knowledgePoints += 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[18].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedReward500(){
+		KnowledgePoints.knowledgePoints += 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[19].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiReward100(){
+		KnowledgePoints.knowledgePoints += 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[20].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiReward200(){
+		KnowledgePoints.knowledgePoints += 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[21].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiReward300(){
+		KnowledgePoints.knowledgePoints += 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[22].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiReward400(){
+		KnowledgePoints.knowledgePoints += 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[23].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiReward500(){
+		KnowledgePoints.knowledgePoints += 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[24].SetActive(false);
+	}
+
+
+	public void GeneralLose100(){
+		KnowledgePoints.knowledgePoints -= 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[0].SetActive(false);
+	}
+
+	public void GeneralLose200(){
+		KnowledgePoints.knowledgePoints -= 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[1].SetActive(false);
+	}
+
+	public void GeneralLose300(){
+		KnowledgePoints.knowledgePoints -= 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[2].SetActive(false);
+	}
+
+	public void GeneralLose400(){
+		KnowledgePoints.knowledgePoints -= 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[3].SetActive(false);
+	}
+
+	public void GeneralLose500(){
+		KnowledgePoints.knowledgePoints -= 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[4].SetActive(false);
+	}
+
+	public void FoundersLose100(){
+		KnowledgePoints.knowledgePoints-= 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[5].SetActive(false);
+	}
+
+	public void FoundersLose200(){
+		KnowledgePoints.knowledgePoints -= 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[6].SetActive(false);
+	}
+
+	public void FoundersLose300(){
+		KnowledgePoints.knowledgePoints -= 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[7].SetActive(false);
+	}
+
+	public void FoundersLose400(){
+		KnowledgePoints.knowledgePoints -= 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[8].SetActive(false);
+	}
+
+	public void FoundersLose500(){
+		KnowledgePoints.knowledgePoints -= 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[9].SetActive(false);
+	}
+
+	public void TheJordenStandardLose100(){
+		KnowledgePoints.knowledgePoints -= 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[10].SetActive(false);
+	}
+
+	public void TheJordenStandardLose200(){
+		KnowledgePoints.knowledgePoints -= 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[11].SetActive(false);
+	}
+
+	public void TheJordenStandardLose300(){
+		KnowledgePoints.knowledgePoints -= 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[12].SetActive(false);
+	}
+
+	public void TheJordenStandardLose400(){
+		KnowledgePoints.knowledgePoints -= 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[13].SetActive(false);
+	}
+
+	public void TheJordenStandardLose500(){
+		KnowledgePoints.knowledgePoints -= 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[14].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedLose100(){
+		KnowledgePoints.knowledgePoints -= 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[15].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedLose200(){
+		KnowledgePoints.knowledgePoints -= 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[16].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedLose300(){
+		KnowledgePoints.knowledgePoints -= 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[17].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedLose400(){
+		KnowledgePoints.knowledgePoints -= 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[18].SetActive(false);
+	}
+
+	public void TheSigmaChiCreedLose500(){
+		KnowledgePoints.knowledgePoints -= 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[19].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiLose100(){
+		KnowledgePoints.knowledgePoints -= 100;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[20].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiLose200(){
+		KnowledgePoints.knowledgePoints -= 200;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[21].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiLose300(){
+		KnowledgePoints.knowledgePoints -= 300;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[22].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiLose400(){
+		KnowledgePoints.knowledgePoints -= 400;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[23].SetActive(false);
+	}
+
+	public void TheSpiritOfSigmaChiLose500(){
+		KnowledgePoints.knowledgePoints -= 500;
+		questionPanel.SetActive (false);
+		mainCanvas.gameObject.SetActive(true);
+		questions[24].SetActive(false);
 	}
 }
---
+++ SigQuiz/Assets/Scripts/KnowledgePoints.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KnowledgePoints : MonoBehaviour {
+
+	public static int knowledgePoints;
+
+	[SerializeField]
+	Text text ;
+
+	// Use this for initialization
+	void Start () {
+		knowledgePoints = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "" + knowledgePoints;
+	}
+}
